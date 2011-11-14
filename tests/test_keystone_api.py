629db013cae95d23515310e43e81fac868de8f83
Jason Cannavale <jason.cannavale@rackspace.com>
2011-11-14 14:14:15 -0600

    Add keystone extension list test.

--- tests/test_keystone_api.py
+++ tests/test_keystone_api.py
@@ -32,7 +32,7 @@ from pprint import pprint
 
 
 class TestKeystoneAPI(tests.FunctionalTest):
-    def test_010_bad_user_bad_key(self):
+    def test_001_bad_user_bad_key(self):
         path = self.nova['path']
         http = httplib2.Http()
         if not self.keystone:
@@ -49,9 +49,9 @@ class TestKeystoneAPI(tests.FunctionalTest):
                                 body,
                                 headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status, 401)
-    test_010_bad_user_bad_key.tags = ['nova', 'nova-api', 'keystone']
+    test_001_bad_user_bad_key.tags = ['nova', 'nova-api', 'keystone']
 
-    def test_011_bad_user_good_key(self):
+    def test_002_bad_user_good_key(self):
         path = self.nova['path']
         http = httplib2.Http()
         if not self.keystone:
@@ -68,9 +68,9 @@ class TestKeystoneAPI(tests.FunctionalTest):
                                 body,
                                 headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status, 401)
-    test_011_bad_user_good_key.tags = ['nova', 'nova-api', 'keystone']
+    test_002_bad_user_good_key.tags = ['nova', 'nova-api', 'keystone']
 
-    def test_012_good_user_bad_key(self):
+    def test_003_good_user_bad_key(self):
         path = self.nova['path']
         http = httplib2.Http()
         if not self.keystone:
@@ -87,9 +87,9 @@ class TestKeystoneAPI(tests.FunctionalTest):
                                 body,
                                 headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status, 401)
-    test_012_good_user_bad_key.tags = ['nova', 'nova-api', 'keystone']
+    test_003_good_user_bad_key.tags = ['nova', 'nova-api', 'keystone']
 
-    def test_013_no_key(self):
+    def test_004_no_key(self):
         path = self.nova['path']
         http = httplib2.Http()
         if not self.keystone:
@@ -105,9 +105,9 @@ class TestKeystoneAPI(tests.FunctionalTest):
                                 body,
                                 headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status, 401)
-    test_013_no_key.tags = ['nova', 'nova-api', 'keystone']
+    test_004_no_key.tags = ['nova', 'nova-api', 'keystone']
 
-    def test_014_bad_token(self):
+    def test_005_bad_token(self):
         path = self.nova['path']
         http = httplib2.Http()
         if not self.keystone:
@@ -123,9 +123,9 @@ class TestKeystoneAPI(tests.FunctionalTest):
                                 body,
                                 headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status, 401)
-    test_014_bad_token.tags = ['nova', 'nova-api', 'keystone']
+    test_005_bad_token.tags = ['nova', 'nova-api', 'keystone']
 
-    def test_015_no_tenant(self):
+    def test_006_no_tenant(self):
         path = self.nova['path'] + "/tokens"
         http = httplib2.Http()
         body = {"passwordCredentials": {
@@ -137,12 +137,12 @@ class TestKeystoneAPI(tests.FunctionalTest):
                             body,
                             headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status, 401)
-    test_015_no_tenant.tags = ['nova', 'nova-api', 'keystone']
+    test_006_no_tenant.tags = ['nova', 'nova-api', 'keystone']
 
     # @tests.skip_test("Currently Not Working")
-    def test_016_get_tenant_list(self):
+    def test_007_get_tenant_list(self):
         path = "http://%s:%s/%s/tenants" % (self.keystone['host'],
-                                           self.keystone['port'],
+                                           self.keystone['admin_port'],
                                            self.keystone['apiver'])
         http = httplib2.Http()
         response, content = http.request(path,
@@ -150,4 +150,19 @@ class TestKeystoneAPI(tests.FunctionalTest):
                             headers={'Content-Type': 'application/json',
                                  'X-Auth-Token': self.nova['X-Auth-Token']})
         self.assertEqual(response.status, 200)
-    test_016_get_tenant_list.tags = ['nova', 'nova-api', 'keystone']
+    test_007_get_tenant_list.tags = ['nova', 'nova-api', 'keystone']
+
+    def test_008_get_extension_list(self):
+        path = "http://%s:%s/%s/extensions" %(self.keystone['host'],
+                                              self.keystone['admin_port'],
+                                              self.keystone['apiver'])
+        http = httplib2.Http()
+        response, content = http.request(path,
+                            'GET',
+                            headers={'Content-Type': 'application/json',
+                                     'X-Auth-Token': self.nova['X-Auth-Token']
+                                    })
+        self.assertEqual(response.status, 200)
+        json_return = json.loads(content)
+        self.assertEqual(json_return['extensions']['values'], [])
+    test_008_get_extension_list.tags = ['nova', 'nova-api', 'keystone']
