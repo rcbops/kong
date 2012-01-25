49b2fd4d1ab446a48acb65e5fa28452798a6d662
William Kelly <the.william.kelly@gmail.com>
2012-01-25 01:30:27 -0600

    KongRequester

--- lib/utils.py
+++ lib/utils.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
 
 from resttest.jsonrequester import JSONRequester
-
-def print_it(*args):
-    from pprint import pprint
-    for a in args:
-        pprint(a)
-    return args
-
+from kongrequester import print_it
 r= JSONRequester(request_transformers=[print_it],
                  response_transformers=[print_it])
