38c332a61fdcb0eeeb9acbacdf7e78e61583306e
William Kelly <the.william.kelly@gmail.com>
2012-01-26 17:34:11 -0600

    pep8

--- lib/resttest/jsontools.py
+++ lib/resttest/jsontools.py
@@ -4,6 +4,7 @@ import json
 from copy import copy
 from httptools import wrap_headers
 
+
 def nested_get(key, data):
     indexes = key.strip("/").split("/")
     r = copy(data)
@@ -16,21 +17,21 @@ def nested_get(key, data):
             r = r[i]
     return r
 
-def safe_nested_get(key,data):
+
+def safe_nested_get(key, data):
     try:
-        return nested_get(key,data)
+        return nested_get(key, data)
     except:
         return None
 
-def nested_search(key,data, acc=None):
+
+def nested_search(key, data, acc=None):
     indexes = key.strip("/").split("/")
-    
     head, rest = indexes[0], "/".join(indexes[1:])
 
     if acc == None:
         # first run
         acc = [data]
-    
     if head == '':
         return acc
     elif head == '*':
@@ -44,7 +45,7 @@ def nested_search(key,data, acc=None):
     elif head.find("=") >= 0:
         #special case 2: SEARCH
         collect = []
-        k,v = head.split("=",1)
+        k, v = head.split("=", 1)
         for i in acc:
             value = safe_nested_get(k, i)
             if v == value:
@@ -52,38 +53,48 @@ def nested_search(key,data, acc=None):
         acc = collect
     else:
         #we need to iterate through acc and remove items that no longer match
-        acc = [ safe_nested_get(head,d) for d in acc if safe_nested_get(head, d) != None]
-    return nested_search(rest,data, acc)
+        acc = [safe_nested_get(head, d) for d in acc if
+               safe_nested_get(head, d) != None]
 
-    
+    return nested_search(rest, data, acc)
+
+
+def nested_match(key, data):
+    return [d for d in data if len(nested_search(key, data)) != 0]
+
 
 class with_keys_op(object):
-    def __init__(self, d, op=lambda x,y: x == y, 
+    def __init__(self, d, op=lambda x, y: x == y,
                  error="json value at key not equal to provide value"):
-        self.d = d 
+        self.d = d\
         self.error = error
         self.op = op
+
     def __call__(self, response, data):
-        jv  = None
+        jv = None
         r = False
-        for k,v in self.d.iteritems():
+        for k, v in self.d.iteritems():
             jv = nested_get(k, data)
             r = apply(self.op, (jv, v))
             if not r:
-                self.error = "json value %s at %s failed comparison against %s" % \
-                    (jv, k,v)
+                self.error = ("json value %s at %s failed comparison " +
+                              "against %s") % (jv, k, v)
         return r
+
 
 def with_keys_eq(d):
     return with_keys_op(d)
 
-def with_keys_ne(d):
-    return with_keys_op(d,op=lambda x,y: x != y)
 
-def json_response(response,data):
+def with_keys_ne(d):
+    return with_keys_op(d, op=lambda x, y: x != y)
+
+
+def json_response(response, data):
     if data != None and data != "":
         return response, json.loads(data)
     return response, data
+
 
 def json_request(uri, method, headers, body, redirections, connection_type):
     nuri, nmethod, nheaders, nbody, nredirections, nconnection_type = \
