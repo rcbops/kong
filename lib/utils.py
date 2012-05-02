0d20b34b55987124ce0783d62ff55b0b948c51bb
William Kelly <the.william.kelly@gmail.com>
2012-05-02 08:56:06 -0500

    pep8, initialize one-off keystone convenience names in SERVICES

--- lib/utils.py
+++ lib/utils.py
@@ -12,20 +12,20 @@ for service in (("image", "glance"), ("compute", "nova"),
     try:
         s, aliases = service[0], service[1:]
         SERVICES[s] = KongRequester(s)
-    except (ValueError,socket.error):
+    except (ValueError, socket.error):
         #no endpoint
         SERVICES[s] = None
     finally:
         try:
             for alias in aliases:
                 SERVICES[alias] = SERVICES[s]
-        except (ValueError,socket.error):
+        except (ValueError, socket.error):
             SERVICES[alias] = None
-                
 
 #one off keystone-admin
 try:
     SERVICES['identity-admin'] = KongRequester('identity', target='adminURL')
     SERVICES['keystone-admin'] = SERVICES['identity-admin']
-except (ValueError,socket.error):
-    pass
+except (ValueError, socket.error):
+    SERVICES['identity-admin'] = None
+    SERVICES['keystone-admin'] = None
