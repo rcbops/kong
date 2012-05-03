bfabf6db042f395299b560dfae0a0b19c9e1848d
Darren Birkett <darren.birkett@gmail.com>
2012-05-03 16:23:18 +0000

    added a nova test under kongrequester framework

--- tests/test_nova_api_2.py
+++ tests/test_nova_api_2.py
@@ -12,3 +12,11 @@ class TestNovaAPI2(tests.FunctionalTest):
         r, d = nova.GET("/flavors", code=200)
         if len(d['flavors']) == 0:
             raise AssertionError("No flavors configured in openstack")
+
+
+    def test_nova_list_servers(self):
+        r, d = nova.GET("/servers", code=200)
+        if len(d['servers']) == 0:
+            raise AssertionError("No servers found at all")
+        elif len(nested_search("/servers/*/name=testserver", d)) < 1:
+            raise AssertionError("Could not find your test server")
