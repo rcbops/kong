4d8e9f0f4716e18211cecfc42559f72f4c8c1532
Darren Birkett <darren.birkett@gmail.com>
2012-11-12 11:36:44 +0000

    more cinder tests

--- tests/test_cinder_api.py
+++ tests/test_cinder_api.py
@@ -6,34 +6,34 @@ from resttest.jsontools import nested_search
 cinder = SERVICES['volume']
 k = SERVICES['keystone']
 user = k.get_config()[1]
-#    absolute-limits     Print a list of absolute limits for a user
-#    create              Add a new volume.
-#    credentials         Show user credentials returned from auth
+##    absolute-limits     Print a list of absolute limits for a user
+##    create              Add a new volume.
+##    credentials         Show user credentials returned from auth
 #    delete              Remove a volume.
-#    endpoints           Discover endpoints that get returned from the
+##    endpoints           Discover endpoints that get returned from the
 #                        authenticate services
-#    list                List all the volumes.
+##    list                List all the volumes.
 #    quota-class-show    List the quotas for a quota class.
 #    quota-class-update  Update the quotas for a quota class.
-#    quota-defaults      List the default quotas for a tenant.
-#    quota-show          List the quotas for a tenant.
-#    quota-update        Update the quotas for a tenant.
-#    rate-limits         Print a list of rate limits for a user
-#    show                Show details about a volume.
+##    quota-defaults      List the default quotas for a tenant.
+##    quota-show          List the quotas for a tenant.
+##    quota-update        Update the quotas for a tenant.
+##    rate-limits         Print a list of rate limits for a user
+##    show                Show details about a volume.
 #    snapshot-create     Add a new snapshot.
 #    snapshot-delete     Remove a snapshot.
-#    snapshot-list       List all the snapshots.
+##    snapshot-list       List all the snapshots.
 #    snapshot-show       Show details about a snapshot.
-#    type-create         Create a new volume type.
+##    type-create         Create a new volume type.
 #    type-delete         Delete a specific flavor
-#    type-list           Print a list of available 'volume types'.
+##    type-list           Print a list of available 'volume types'.
 
 
 class TestCinderAPI(tests.FunctionalTest):
     tags = ['cinder']
 
     def test_001_get_absolute_limits(self):
-        cinder.GET('/limits?format=json', code=200)
+        cinder.GET('/limits', code=200)
 
     def test_002_list_volumes(self):
         cinder.GET('/volumes/detail', code=200)
@@ -44,104 +44,61 @@ class TestCinderAPI(tests.FunctionalTest):
     def test_004_list_current_quotas(self):
         cinder.GET('/os-quota-sets/%s' % user, code=200)
 
-    def test_005_update_quotas(self):
+    def test_005_list_rate_limits(self):
+        cinder.GET('/limits', code=200)
+
+    def test_006_list_types(self):
+        cinder.GET('/types', code=200)
+        
+    def test_007_list_snapshots(self):
+        cinder.GET('/snapshots/detail', code=200)
+
+
+    def test_008_update_quotas(self):
         resp, body = cinder.GET('/os-quota-sets/%s' % user, code=200)
         current_volumes = body['quota_set']['volumes']
         target_volumes = current_volumes +1
 
         cinder.PUT("/os-quota-sets/%s" % user,
                   body={"quota_set":
-                        {"tenant_id": "%s" % user,
-                         "volumes": "%s" % target_volumes}},
-                         code=200)
+                       {"tenant_id": "%s" % user,
+                        "volumes": "%s" % target_volumes}},
+                        code=200)
 
         resp, body = cinder.GET('/os-quota-sets/%s' % user, code=200)
         new_volumes = body['quota_set']['volumes']
 
         if new_volumes != target_volumes:
-            raise AssertionError('no way buddy %s' % current_volumes)
+            raise AssertionError('Could not update quotas')
 
+    def test_009_create_volume_type(self):
+        cinder.POST('/types', body={"volume_type":
+                                   {"name": "test-type"}}, code=200)
 
+    def test_010_create_volume(self):
+        resp, body = cinder.POST('/volumes', body={"volume":
+                                                  {"size": "1",
+                                                   "volume_type": "test-type",
+                                                   "display_name": "test-volume"}},
+                                                   code=200)
+        volume_id = body["volume"]["id"]
 
-#    def test_003_list_containers(self):
-#        response, body = swift.GET('?format=json', code=200)
-#        #check to see container count is greater than zero
-#        if int(response['x-account-container-count']) < 1:
-#            raise AssertionError("No containers found")
-#
-#    def test_004_create_custom_container_meta(self):
-#        headers = {"X-Container-Meta-blah": "blahblah"}
-#        swift.POST('/%s?format=json' % (CONTAINER), headers=headers, code=204)
-#
-## create objects
-#
-#    def test_005_create_normal_object(self):
-#        # open fake file object
-#        obj = StringIO.StringIO()
-#        # write some Content
-#        obj.write(NORMAL_OBJ)
-#        headers = ({'Content-Length': '%d' % obj.len,
-#                'Content-Type': 'application/octet-stream'})
-#        swift.PUT_raw('/%s/%s' % (CONTAINER, NORMAL_OBJ),
-#                headers=headers, body=obj.getvalue(),  code=201)
-#
-#    def test_006_create_manifest_object(self):
-#        # create 3 objects
-#        for obj in [MULTIPART_OBJ_1, MULTIPART_OBJ_2, MULTIPART_OBJ_3]:
-#            p = StringIO.StringIO()
-#            p.write(obj)
-#            headers = ({'Content-Length': '%d' % p.len,
-#                'Content-Type': 'application/octet-stream'})
-#            swift.PUT_raw('/%s/%s/%s' % (CONTAINER, PREFIX, obj),
-#                headers=headers, body=p.getvalue(),  code=201)
-#
-#        # create the manifest file
-#        headers = ({'X-Object-Manifest': '%s/%s' % (CONTAINER, PREFIX),
-#                'Content-Length': '0'})
-#        swift.PUT_raw('/%s/%s' % (CONTAINER, PREFIX),
-#                headers=headers, code=201)
-#
-## update object Meta
-#    def test_008_create_custom_object_meta(self):
-#        headers = ({'X-Object-Meta-blah': 'blahblah'})
-#        swift.POST('/%s/%s?format=json' % (CONTAINER, NORMAL_OBJ),
-#                headers=headers, code=202)
-#
-## get objects
-#    def test_009_get_normal_object(self):
-#        response, body = swift.GET_raw('/%s/%s?format=json'
-#                % (CONTAINER, NORMAL_OBJ), code=200)
-#        if body != NORMAL_OBJ:
-#            raise AssertionError('file does not contain expected contents')
-#
-#    def test_010_get_manifest_object(self):
-#        response, body = swift.GET_raw('/%s/%s'
-#                % (CONTAINER, PREFIX), code=200)
-#        if body != "".join([MULTIPART_OBJ_1, MULTIPART_OBJ_2,
-#            MULTIPART_OBJ_3]):
-#            raise AssertionError('file does not contain expected contents')
-#
-#    def test_012_delete_normal_object(self):
-#        swift.DELETE('/%s/%s?format=json' % (CONTAINER, NORMAL_OBJ),
-#                     code=204)
-#
-#    def test_013_delete_manifest_object(self):
-#        # delete manifest file first
-#        swift.DELETE('/%s/%s?format=json' % (CONTAINER, PREFIX),
-#                     code=204)
-#        # delete all parts individually
-#        for m in [MULTIPART_OBJ_1, MULTIPART_OBJ_2, MULTIPART_OBJ_3]:
-#            swift.DELETE('/%s/%s/%s' % (CONTAINER, PREFIX,
-#                    m), code=204)
-#
-#    def test_100_delete_container(self):
-#        # need to get a list of objects in the container and delete them
-#        response, body = swift.GET(('/%s?format=json') % (CONTAINER), code=200)
-#        # pull the objects out of the returned json
-#        objects = nested_search('*/name', body)
-#        # delete the objects one by one
-#        for obj in objects:
-#            swift.DELETE('%s/%s' % (CONTAINER, obj), code=204)
-#
-#        # now we can delete the container
-#        swift.DELETE('/%s?format=json' % (CONTAINER), code=204)
+        resp, body = cinder.GET_with_keys_eq('/volumes/%s' % volume_id,
+                                            {"/volume/status": "available"},
+                                            code=200, timeout=60, delay=5)
+
+    def test_011_create_snapshot(self):
+        volume_id = nested_search('/volumes/*/display_name=test-volume/id',
+                        cinder.GET('/volumes/detail')[1])[0]
+
+        resp, body = cinder.POST('/snapshots', body={"snapshot":
+                                                     {"display_name": "test-snapshot",
+                                                     "volume_id": volume_id}},
+                                                     code=200)
+
+        snapshot_id = body["snapshot"]["id"]
+
+        resp, body = cinder.GET_with_keys_eq('/snapshots/%s' % snapshot_id,
+                                            {"/snapshot/status": "available"},
+                                            code=200, timeout=60, delay=5)
+
