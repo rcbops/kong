0ea63ff1393b02b718fd6c26503ba7be1816433a
Darren Birkett <darren.birkett@gmail.com>
2013-08-09 10:20:24 +0000

    almost working nova-neutron tests

--- tests/test_nova_api.py
+++ tests/test_nova_api.py
@@ -233,6 +233,67 @@ class TestNovaAPI(tests.FunctionalTest):
 
     test_161_security_group_rule_create.tags = ['nova-neutron']
 
+    def test_165_net_create(self):
+        resp, body = neutron.POST(
+            '/%s/networks.json' % neutron_api_ver,
+            body={'network': {
+                  'name': self.config['nova']['network_label'],
+                  'admin_state_up': 'True'}},
+            code=201)
+
+        network_id = body['network']['id']
+
+        resp, body = neutron.GET_with_keys_eq(
+            '/%s/networks/%s' % (neutron_api_ver, network_id),
+            {'/network/status': 'ACTIVE'},
+            code=200, timeout=20, delay=2)
+
+    def test_170_subnet_create(self):
+        network_id = nested_search(
+            'networks/*/name=%s/id' % self.config['nova']['network_label'],
+            neutron.GET('/%s/networks' % neutron_api_ver, code=200)[1])[0]
+
+        resp, body = neutron.POST(
+            '/%s/subnets.json' % neutron_api_ver,
+            body={'subnet': {
+                  'network_id': '%s' % network_id,
+                  'ip_version': 4,
+                  'cidr': '192.168.78.0/29',
+                  'name': 'test-subnet'}},
+            code=201)
+
+    def test_190_create_server_neutron(self):
+        network_id = nested_search(
+            'networks/*/name=%s/id' % self.config['nova']['network_label'],
+            neutron.GET('/%s/networks' % neutron_api_ver, code=200)[1])[0]
+        image = nova.GET("/images?name=test-image")[1]['images'][0]['id']
+        flavor = nested_search("/flavors/*/name=m1.tiny/id",
+                               nova.GET("/flavors")[1])[0]
+
+
+        r, d = nova.POST("/servers",
+                        body={"server": 
+                                 {"name": "testing server creation", 
+                                 "imageRef": image, "flavorRef": flavor, 
+                                 "max_count": 1, "min_count": 1, 
+                                 "networks": [{"uuid": network_id}]
+                                 }
+                             },
+                        code=202) 
+        server_id = d['server']['id']
+        r, d = nova.GET_with_keys_eq("/servers/%s" % server_id,
+                                     {"/server/status": "ACTIVE"},
+                                     code=200, timeout=60, delay=5)
+        ip = d['server']['addresses'][self.config['nova']['network_label']][0]['addr']
+
+        if not self.ping_neutron_host(ip, delay=5, timeout=200):
+            raise AssertionError("Server is active but does not ping")
+
+
+
+    test_190_create_server_neutron.tags = ['nova-neutron']
+
+
     def test_200_create_server(self):
         image = nova.GET("/images?name=test-image")[1]['images'][0]['id']
         flavor = nested_search("/flavors/*/name=m1.tiny/id",
@@ -263,14 +324,14 @@ class TestNovaAPI(tests.FunctionalTest):
             raise AssertionError("No server found with name testing server " +
                                  "creation")
 
-    test_201_list_servers.tags = ['nova']
+#    test_201_list_servers.tags = ['nova']
 
     def test_202_get_server_details(self):
         r, d = nova.GET("/servers/detail")
         sid = nested_search("/servers/*/name=testing server creation/id", d)[0]
         r, d = nova.GET("/servers/%s" % sid, code=200)
 
-    test_202_get_server_details.tags = ['nova']
+#    test_202_get_server_details.tags = ['nova']
 
     def test_203_update_server(self):
         r, d = nova.GET("/servers/detail")
@@ -285,7 +346,7 @@ class TestNovaAPI(tests.FunctionalTest):
                  body={"server":
                        {"name": "testing server creation"}})
 
-    test_203_update_server.tags = [ 'nova']
+#    test_203_update_server.tags = [ 'nova']
 
     def test_210_list_addresses(self):
         label = self.config['nova']['network_label']
@@ -296,7 +357,7 @@ class TestNovaAPI(tests.FunctionalTest):
         if not len(nested_search('/addresses/%s' % (label), addrs)[0]) > 0:
             raise AssertionError("No addresses found for network %s" % (label))
 
-    test_210_list_addresses.tags = ['nova']
+#    test_210_list_addresses.tags = ['nova']
 
     def test_300_delete_server(self):
         r, d = nova.GET("/servers/detail")
@@ -305,7 +366,7 @@ class TestNovaAPI(tests.FunctionalTest):
         r, d = nova.GET("/servers/%s" % sid,
             code=404, timeout=60, delay=5)
 
-    test_300_delete_server.tags = ['nova']
+#    test_300_delete_server.tags = ['nova']
 
     def test_305_delete_test_images_from_glance(self):
         for name in ["test-kernel", "test-ramdisk", "test-image"]:
@@ -315,7 +376,7 @@ class TestNovaAPI(tests.FunctionalTest):
                 "/images/%s" % i,
                 {"/image/status": "DELETED"},
                 code=200, timeout=60, delay=5)
-    test_305_delete_test_images_from_glance.tags = ['glance', 'nova']
+    test_305_delete_test_images_from_glance.tags = ['glance', 'nova', 'nova-neutron']
 
     def test_901_delete_security_group_rule(self):
         data = nova.GET("/os-security-groups")[1]
@@ -390,3 +451,14 @@ class TestNovaAPI(tests.FunctionalTest):
         nova.DELETE("/os-security-groups/%s" % gid, timeout=60,
                     delay=5, code=404)
     test_955_delete_security_group_diablo_final.tags = ['nova']
+
+    def test_960_net_delete(self):
+        resp, body = neutron.GET(
+            '/%s/networks.json?fields=id&name=%s' 
+                % (neutron_api_ver, self.config['nova']['network_label']), code=200)
+        network_ids = [p['id'] for p in body['networks']]
+
+        for net in network_ids:
+            neutron.DELETE('/%s/networks/%s.json' % (neutron_api_ver, net), code=204)
+
+    test_960_net_delete.tags = ['nova-neutron']
